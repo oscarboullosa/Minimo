c00094e7a9dff83d9eeccb3d459d1780d69b5029
oscarboullosa <oscar.boullosa@estudiantat.upc.edu>
2022-11-11 11:47:05 +0100

    Clase Game

--- src/main/java/edu/upc/dsa/minimo/Domain/Entity/Game.java
+++ src/main/java/edu/upc/dsa/minimo/Domain/Entity/Game.java
@@ -1,4 +1,59 @@
 package edu.upc.dsa.minimo.Domain.Entity;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Game {
+    String gameId;
+    String gameName;
+    String description;
+    int gameLevelNumber;
+    LinkedList<Level> gameLevels;
+
+    public Game(String gameId, String description, int gameLevelNumber) {
+        this.gameId = gameId;
+        this.description = description;
+        this.gameLevelNumber = gameLevelNumber;
+        this.gameLevels=new LinkedList<>();
+    }
+
+    public String getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(String gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getGameLevelNumber() {
+        return gameLevelNumber;
+    }
+
+    public void setGameLevelNumber(int gameLevelNumber) {
+        this.gameLevelNumber = gameLevelNumber;
+    }
+
+    public LinkedList<Level> getGameLevels() {
+        return gameLevels;
+    }
+
+    public void setGameLevels(LinkedList<Level> gameLevels) {
+        this.gameLevels = gameLevels;
+    }
 }
--- src/main/java/edu/upc/dsa/minimo/Domain/Entity/User.java
+++ src/main/java/edu/upc/dsa/minimo/Domain/Entity/User.java
@@ -7,4 +7,5 @@ public class User {
     Game userGame;
 
 
+
 }
